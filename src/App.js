1441b03cd4ecd2a4742bdebd330323d0e9e27052
Farver <mfarver@us7102084-m001.clients.us.kworld.kpmg.com>
2019-01-29 13:30:45 -0500

    modified app text

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> Hello Matt. Oh yea this is cool I was
-            changed
+            Edit <code>src/App.js</code> Hello Matt. I am now using
+            elasticbeanstalk
           </p>
           <a
             className="App-link"
